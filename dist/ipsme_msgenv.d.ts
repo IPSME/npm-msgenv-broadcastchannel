4c7a90a27c196152892aedb6a4db8443bdbb24aa
knev <knev@users.noreply.github.com>
2026-07-23 11:59:24 +0200

    chore: bump version.

--- dist/ipsme_msgenv.d.ts
+++ dist/ipsme_msgenv.d.ts
@@ -1,8 +1,3 @@
-declare const l_: {
-    MsgEnv: number;
-    CXNS: number;
-    REFL: number;
-};
 declare var cfg_: {
     readonly channel: any;
     readonly prefix: any;
@@ -11,5 +6,6 @@ declare var cfg_: {
 declare function subscribe(handler: any): void;
 declare function unsubscribe(handler: any): void;
 declare function publish(msg: any): void;
+declare const logr_: any;
 
-export { cfg_ as config, l_ as l, publish, subscribe, unsubscribe };
+export { cfg_ as config, logr_ as logr, publish, subscribe, unsubscribe };
